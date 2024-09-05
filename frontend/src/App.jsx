62d2d5d1e47af84113d44a898a19db229b393cbd
kunal-2308 <kunaltaware210@gmail.com>
2024-09-05 22:50:16 +0530

    Begining of FrontEnd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='main-title font-extrabold text-slate-300'>GANAPATI BAPPA MORYA</h1>
+    </>
+  )
+}
+
+export default App
